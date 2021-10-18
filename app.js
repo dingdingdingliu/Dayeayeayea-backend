c44aa924c3014d54db73b3536adccf96b9abee7c
CoolizzLuo <duenzo1010@gmail.com>
2021-10-18 15:52:30 +0800

    fix logger, app.js

--- app.js
+++ app.js
@@ -30,9 +30,7 @@ app.use('/products', productsRouter)
 app.use('/faq', faqController.getAll)
 app.use('/admin', adminRouter)
 
-if (process.env.NODE_ENV === 'development') {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-}
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(/\//, (req, res) => res.json({ ok: 1, message: 'Welcome DAYEAYEAYEA' }))
 app.use('/*', (req, res, next) => next(createError(404)))
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -4,7 +4,7 @@ const rt = require("file-stream-rotator")
 
 
 morgan.token('date', (req, res, tz) => moment().tz(tz).format('HH:mm:ss'))
-morgan.token('access', (req, res) => req.auth ? `${req.auth.memberId}` : 'Guest')
+morgan.token('access', (req, res) => req.auth ? `${req.auth.username}` : 'Guest')
 // :remote-addr
 morgan.format('ozne', ':date[Asia/Taipei] -- :status - :access - :method :url :response-time ms :res[content-length] \t\t :user-agent')
 

754489435b068bc34fe7e983f8678e1f4bcb1019
CoolizzLuo <duenzo1010@gmail.com>
2021-11-11 14:09:15 +0800

    add route /category/all

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,14 @@ const router = express.Router()
 const Products = require('../controller/products')
 
 
-const categoryMatch = 'home|apparel|kitchenware|food|stationery|outdoor'
+const categoryMatch = 'all|home|apparel|kitchenware|food|stationery|outdoor'
 const articleMatch = 'null|dining|fragrance|camping'
 // 搜尋商品 ?search=keyword
 router.get('/', Products.getAll)
 router.get('/:id([0-9]+)', Products.getOne)
 router.get('/page/:page([1-9]|[1-9][0-9]+)', Products.getByPage)
 
+router.get(`/category/all/:page([1-9]|[1-9][0-9])?`, Products.getByPage)
 router.get(`/category/:category(${categoryMatch})/:page([1-9]|[1-9][0-9])?`, Products.getByCategory)
 router.get(`/article/:article(${articleMatch})/:page([1-9]|[1-9][0-9]+)?`, Products.getByArticle)
 
